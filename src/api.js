c70bbf4bf78150e0d675bd60a55d7c78804c16ce
Piyush Kumar <24145@iiitu.ac.in>
2025-10-16 16:58:25 +0530

    hekko

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Base URL from env variable, fallback Render URL
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "https://portfolio-nodemailer-78y1.onrender.com",
+  baseURL: process.env.REACT_APP_API_BASE_URL1 || "https://portfolio-nodemailer-78y1.onrender.com",
 });
 
 export default api;
